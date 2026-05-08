a925e8d0b57087c55f77605ec0ac32234317772a
BoronteIstvan <boronte.isti03@gmail.com>
2026-05-08 19:30:40 +0200

    Add files via upload

---
+++ logicals/belep.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if(isset($_POST['felhasznalo']) && isset($_POST['jelszo'])) {
+    try {
+        $dbh = new PDO('mysql:host=mysql.omega;dbname=fel12', 'fel12', 'Asd123asd',
+                array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
+        
+
+        $sqlSelect = "select id, csaladi_nev, uto_nev from felhasznalok where bejelentkezes = :bejelentkezes and jelszo = sha1(:jelszo)";
+        $sth = $dbh->prepare($sqlSelect);
+        $sth->execute(array(':bejelentkezes' => $_POST['felhasznalo'], ':jelszo' => $_POST['jelszo']));
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+        if($row) {
+            $_SESSION['csn'] = $row['csaladi_nev']; $_SESSION['un'] = $row['uto_nev']; $_SESSION['login'] = $_POST['felhasznalo'];
+        }
+    }
+    catch (PDOException $e) {
+        $errormessage = "Hiba: ".$e->getMessage();
+    }      
+}
+else {
+    header("Location: .");
+}
+?>
